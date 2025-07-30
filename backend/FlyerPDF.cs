177380e70b370b7a281b0dcaaf5493d0d38caab7
Gados László Márk <gadosmark7@gmail.com>
2025-07-30 15:38:50 +0200

    extend pdf model with firstpageURL attribute

--- backend/FlyerPDF.cs
+++ backend/FlyerPDF.cs
@@ -13,8 +13,12 @@ namespace backend
         public string SupermarketId { get; set; }
 
         [JsonPropertyName("ActualDate")]
-        [BsonElement("AcutalDate"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        [BsonElement("ActualDate"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
         public string ActualDate { get; set; }
+
+        [JsonPropertyName("FirstPageURL")]
+        [BsonElement("FirstPageURL"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
+        public string FirstPageURL { get; set; }
 
         [JsonPropertyName("URL")]
         [BsonElement("URL"), BsonRepresentation(MongoDB.Bson.BsonType.String)]
